1382669e67a67fbab7223447e223b11c34fcb2f4
ElijG <110502349+ElijG@users.noreply.github.com>
2026-06-02 22:39:40 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,59 @@
+const ROUNDS = [
+  
+]
+  {
+    id: "r1p1",
+    username: "example",
+    avatar: "emoji example",
+    location: "example",
+    likes: "1000 ",
+    caption: "example",
+    timestamp: "3 hours ago",
+    isFake: false,
+    fakeclue: "example",
 
+  },
+  {
+    id: "r1p2"
+    username: "example",
+    avatar: "emoji example",
+    location: "example",
+    likes: "1000 ",
+    caption: "example",
+    timestamp: "3 hours ago",
+    isFake: false,
+    fakeclue: "example",
+  },
+  {
+    id: "r1p3"
+    username: "example",
+    avatar: "emoji example",
+    location: "example",
+    likes: "1000 ",
+    caption: "example",
+    timestamp: "3 hours ago",
+    isFake: false,
+    fakeclue: "example",
+  },
+  {
+    id: "r1p4"
+    username: "example",
+    avatar: "emoji example",
+    location: "example",
+    likes: "1000 ",
+    caption: "example",
+    timestamp: "3 hours ago",
+    isFake: false,
+    fakeclue: "example",
+  },
+  {
+    id: "r1p5"
+    username: "example",
+    avatar: "emoji example",
+    location: "example",
+    likes: "1000 ",
+    caption: "example",
+    timestamp: "3 hours ago",
+    isFake: false,
+    fakeclue: "example",
+  },
